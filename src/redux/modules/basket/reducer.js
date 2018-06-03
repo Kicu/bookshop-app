646bc9ab5e4340cb319f7b9f7dfd71fae73792b8
Mateusz Titz <mateusz.titz@schibsted.pl>
2018-06-03 19:40:30 +0200

    Ex7.1 - basket module + Add to basket button

---
+++ src/redux/modules/basket/reducer.js
@@ -0,0 +1,45 @@
+import { ADD_TO_BASKET, REMOVE_FROM_BASKET } from './actionTypes';
+
+function basketReducer(state = [], action) {
+    switch (action.type) {
+        case ADD_TO_BASKET: {
+            const { bookId } = action.payload;
+            const bookIndex = state.findIndex(basketItem => basketItem.bookId === bookId);
+            if (bookIndex > -1) {
+                const book = {
+                    bookId: state[bookIndex].bookId,
+                    quantity: state[bookIndex].quantity + 1
+                };
+
+                return [
+                    ...state.slice(0, bookIndex),
+                    book,
+                    ...state.slice(bookIndex + 1)
+                ];
+            }
+
+            return [...state, { bookId, quantity: 1 }];
+        }
+        case REMOVE_FROM_BASKET:
+            const { bookId } = action.payload;
+            const bookIndex = state.findIndex(basketItem => basketItem.bookId === bookId);
+            if (bookIndex > -1) {
+                return [
+                    ...state.slice(0, bookIndex),
+                    ...state.slice(bookIndex + 1)
+                ]
+            }
+
+            return state;
+
+        default:
+            return state;
+    }
+}
+
+const getBasket = (state) => state.basket;
+
+export default basketReducer;
+export {
+    getBasket
+}

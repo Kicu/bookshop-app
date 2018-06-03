d74d2b28f0eb9c65d4926a6df34e6009dca2bfc3
Mateusz Titz <mateusz.titz@schibsted.pl>
2018-06-03 17:05:20 +0200

    Ex6.2 - 3 - categories and featured reducers

--- src/redux/modules/featured/reducer.js
+++ src/redux/modules/featured/reducer.js
@@ -3,13 +3,13 @@ import { FETCH_FEATURED_SUCCESS } from './actionTypes';
 function featuredReducer(state = [], action) {
     switch (action.type) {
         case FETCH_FEATURED_SUCCESS:
-            return action.payload.featured;
+            return action.payload.featured.map(book => book.id);
         default:
             return state;
     }
 }
 
-const getFeaturedBooks = (state) => state.featured;
+const getFeaturedBooks = (state) => state.featured.map(bookId => state.books[bookId]);
 
 export default featuredReducer;
 export {

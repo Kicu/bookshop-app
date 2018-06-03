2ff6fe809235a509091923a72fe15723b087685a
Mateusz Titz <mateusz.titz@schibsted.pl>
2018-06-03 15:08:51 +0200

    Ex4 Refactor into BookList

---
+++ src/components/BookList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './BookList.css';
+import Book from './Book';
+
+const BookList = ({ books }) => (
+    <div className="BookList">
+        <ul className="BookList-list">
+            {books.map(book => (
+                <li key={book.id} className="BookList-list-item"><Book {...book}/></li>
+            ))}
+        </ul>
+    </div>
+);
+
+BookList.propTypes = {
+    books: PropTypes.array,
+};
+
+export default BookList;

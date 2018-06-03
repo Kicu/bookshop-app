c1b1db6aabfe73a9d3ab2dc925f7d8436f623de8
Mateusz Titz <mateusz.titz@schibsted.pl>
2018-06-03 17:05:20 +0200

    Ex6.2 - 3 - categories and featured reducers

--- src/redux/modules/byCategory/reducer.js
+++ src/redux/modules/byCategory/reducer.js
@@ -3,13 +3,13 @@ import { FETCH_BY_CATEGORY_SUCCESS } from './actionTypes';
 function byCategoryReducer(state = [], action) {
     switch (action.type) {
         case FETCH_BY_CATEGORY_SUCCESS:
-            return action.payload.books;
+            return action.payload.books.map(book => book.id);
         default:
             return state;
     }
 }
 
-const getBooksByCategory = (state) => state.byCategory;
+const getBooksByCategory = (state) => state.byCategory.map(bookId => state.books[bookId]);
 
 export default byCategoryReducer;
 export {

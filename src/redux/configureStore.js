3308abc214faf26c3c60383cc94d23322cb78760
Mateusz Titz <mateusz.titz@schibsted.pl>
2018-06-03 16:58:31 +0200

    Ex6.2 - 1 - `books` normalized reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,11 +4,13 @@ import thunk from 'redux-thunk';
 import featuredReducer from './modules/featured';
 import byCategory from './modules/byCategory';
 import search from './modules/search';
+import books from './modules/books';
 
 const reducer = combineReducers({
     featured: featuredReducer
     byCategory,
-    search
+    search,
+    books
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
